54a618067b97306d42bfc6c6f0fbfaed33afa5c0
vince <vincedinardo21@gmail.com>
2022-05-26 16:56:04 +0200

    problema testo lista

--- myscript.js
+++ myscript.js
@@ -1,24 +1,33 @@
 var myTodoList = new Vue({
     el : "#app",
     data : {
+        newTodo : {
+            text : "",
+            done : true
+        },
         todos : [
             {
-                text : "Fare il bucato",
+                text : "Deep Purple - Smoke on the water",
                 done : false
             },
             {
-                text : "Fare la spesa",
+                text : "Oasis - Stand by me",
                 done : true
             },
             {
-                text : "Andare dal barbiere",
+                text : "Pink Floyd - Another brick in the wall",
+                done : true
+            },
+            {
+                text : "Blink 182 - All the small things",
                 done : true
             }
         ]
     },
     methods : {
         addItem(){
-            
+            this.todos.push(this.newTodo.text);
+            // this.newTodo.text = "";
         }
     }
 });
